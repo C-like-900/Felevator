76ec4663840091c1d52648a8712cec2775fd247e
Си-подобный <119621338+C-like-900@users.noreply.github.com>
2025-01-04 15:58:46 +0700

    Delete Felevator.bat

--- Felevator.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-chcp 1251
-IF %1 == install (
-reg add "HKCR\Directory\Shell\Felevator" /t REG_SZ /d "Извлечь содержимое"
-reg add "HKCR\Directory\Shell\Felevator\command" /t REG_SZ /d "%~dp0Felevator.exe %%1"
-)
-IF %1 == uninstall (
-reg delete "HKCR\Directory\Shell\Felevator" /f
-)
